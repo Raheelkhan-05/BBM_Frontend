711dc24d6e24c22367a3ad008bd386e5c149058d
Raheelkhan-05 <2005raheelkhan@gmail.com>
2026-06-19 17:52:44 +0530

    deployment fixes

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-const API =
-  "http://localhost:5000/api/auth";
+const API = import.meta.env.VITE_API_URL || "http://localhost:5000/api/auth";
 
 export const signupUser =
   async (payload) => {
