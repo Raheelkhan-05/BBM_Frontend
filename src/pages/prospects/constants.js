078fcdd181f9f08ee5fc4637c4b75d43b143116e
Raheelkhan-05 <2005raheelkhan@gmail.com>
2026-07-03 11:45:13 +0530

    Added Mould Work in Industry Options

--- src/pages/prospects/constants.js
+++ src/pages/prospects/constants.js
@@ -1,5 +1,5 @@
 export const INDUSTRIES = [
-  "Pharmaceuticals","Casting",  "Forging",  "Job Work",  "Textiles",  "Chemicals",  "Food & Beverage",  "Automotive",  "Electronics",  "Plastics & Rubber",
+  "Pharmaceuticals","Casting",  "Forging",  "Job Work", "Mould Work", "Textiles",  "Chemicals",  "Food & Beverage",  "Automotive",  "Electronics",  "Plastics & Rubber",
   "Paper & Packaging",  "Construction",  "Agriculture",  "Metal & Mining",  "Oil & Gas",  "Paints & Coatings",  "Adhesives & Sealants",  "Water Treatment",
   "Cosmetics & Personal Care",  "Bearing Manufacturing",  "Auto Component Manufacturing", "Precision Engineering Company",  "Gear Manufacturing", "Shaft Manufacturing",  "Forging Company",
   "Fastener Manufacturing",  "Liner Manufacturing",  "Powder Coating Work",  "Valve Manufacturing",  "Hydraulic Cylinder Manufacturing",  "Door Closer Manufacturing",
